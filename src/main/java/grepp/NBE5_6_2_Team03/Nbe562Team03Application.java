4773a776a466e8bd1b651df4e13e887b1f44e9f7
hyeunS-P <hseric0816@gmail.com>
2025-05-07 10:51:31 +0900

    초기 설정

---
+++ src/main/java/grepp/NBE5_6_2_Team03/Nbe562Team03Application.java
@@ -0,0 +1,13 @@
+package grepp.NBE5_6_2_Team03;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Nbe562Team03Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Nbe562Team03Application.class, args);
+	}
+
+}
---
+++ src/test/java/grepp/NBE5_6_2_Team03/Nbe562Team03ApplicationTests.java
@@ -0,0 +1,13 @@
+package grepp.NBE5_6_2_Team03;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Nbe562Team03ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
