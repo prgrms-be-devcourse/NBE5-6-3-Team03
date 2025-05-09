27d1e24a1ddecde88480cb89ba60c018392613f7
hyeunS-P <hseric0816@gmail.com>
2025-05-09 17:48:38 +0900

    feature: gemini connect

--- src/main/java/grepp/NBE5_6_2_Team03/api/controller/llm/TripAiController.java
+++ src/main/java/grepp/NBE5_6_2_Team03/api/controller/llm/TripAiController.java
@@ -13,7 +13,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping
+@RequestMapping("/api/ai")
 public class TripAiController {
 
     private final ChatLanguageModel chatLanguageModel;
--- src/main/java/grepp/NBE5_6_2_Team03/api/controller/llm/dto/PlaceDto.java
+++ src/main/java/grepp/NBE5_6_2_Team03/api/controller/llm/dto/PlaceDto.java
@@ -2,6 +2,24 @@ package grepp.NBE5_6_2_Team03.api.controller.llm.dto;
 
 public class PlaceDto {
     private String name;
-    private String imageUrl;
+    private String recommend;
     private String address;
+
+    public PlaceDto() {}
+
+    public PlaceDto(String name, String recommend, String address) {
+        this.name = name;
+        this.recommend = recommend;
+        this.address = address;
+    }
+
+    public String getName() {
+        return name;
+    }
+    public String getRecommend() {
+        return recommend;
+    }
+    public String getAddress() {
+        return address;
+    }
 }
