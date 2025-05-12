a9ec68fa1f15b0138192d04fec13b1ab492d7181
NoviceWyatt19 <aseltula596@gmail.com>
2025-05-12 17:04:04 +0900

    chore: parser 삭제

--- src/main/java/grepp/NBE5_6_2_Team03/domain/admin/place/parser/AddressParser.java
+++
@@ -1,9 +0,0 @@
-package grepp.NBE5_6_2_Team03.domain.admin.place.parser;
-
-import com.fasterxml.jackson.databind.JsonNode;
-
-public interface AddressParser {
-    String parsingCountry(JsonNode addressComponents);
-    String parsingCity(JsonNode addressComponents);
-    String extractByType(JsonNode components, String targetType);
-}
