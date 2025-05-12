be39b0c897ff3085ec9900918bf61c4c6f3d06fc
hyeunS-P <hseric0816@gmail.com>
2025-05-12 15:35:57 +0900

    feat: 관광지 추천 채팅 페이지로 redirect

--- src/main/java/grepp/NBE5_6_2_Team03/api/controller/llm/TripAiController.java
+++ src/main/java/grepp/NBE5_6_2_Team03/api/controller/llm/TripAiController.java
@@ -6,7 +6,7 @@ import grepp.NBE5_6_2_Team03.api.controller.llm.dto.PlaceRecommendResponse;
 import grepp.NBE5_6_2_Team03.domain.llm.service.TripAiService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -19,12 +19,9 @@ public class TripAiController {
     private final ChatLanguageModel chatLanguageModel;
     private final TripAiService tripAiService;
 
-
-
-    @GetMapping("/recommend")
-    public ResponseEntity<PlaceRecommendResponse> recommend(@RequestParam String region){
-        PlaceRecommendRequest request = new PlaceRecommendRequest(region);
-        PlaceRecommendResponse response = tripAiService.recommend(request);
+    @PostMapping("/chat")
+    public ResponseEntity<PlaceRecommendResponse> recommend(@RequestParam String message){
+        PlaceRecommendResponse response = tripAiService.recommend(message);
         return ResponseEntity.ok(response);
     }
 }
--- src/main/java/grepp/NBE5_6_2_Team03/global/config/SecurityConfig.java
+++ src/main/java/grepp/NBE5_6_2_Team03/global/config/SecurityConfig.java
@@ -24,7 +24,7 @@ public class SecurityConfig {
                 .authorizeHttpRequests(
                         (auth) -> auth
                                 .requestMatchers("/", "/users/**").permitAll()
-                                .requestMatchers("/css/**", "/assets/**", "/js/**","/api/ai/recommend").permitAll()
+                                .requestMatchers("/css/**", "/assets/**", "/js/**","/api/ai/recommend","/trip-chat").permitAll()
                                 .anyRequest().authenticated()
                 );
 
