b2b8cc742bae5fe05c585a7643c5e93b5e92ff82
NoviceWyatt19 <aseltula596@gmail.com>
2025-05-13 13:05:17 +0900

    test: google place api test 변경(assert 사용 버전으로 변경 못함)

--- src/test/java/grepp/NBE5_6_2_Team03/domain/admin/place/PlaceServiceTest.java
+++ src/test/java/grepp/NBE5_6_2_Team03/domain/admin/place/PlaceServiceTest.java
@@ -2,8 +2,11 @@ package grepp.NBE5_6_2_Team03.domain.admin.place;
 
 import grepp.NBE5_6_2_Team03.domain.admin.place.entity.Place;
 import java.io.IOException;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -12,24 +15,36 @@ import org.springframework.boot.test.context.SpringBootTest;
 public class PlaceServiceTest {
 
     @Autowired
-    private PlaceService placeService;
+    private GooglePlaceService placeService;
 
     @Test
+    @DisplayName(value = "")
     void getPlaceIds() throws IOException {
-        List<String> results = placeService.searchPlaceIds(34.6937,135.5023, 7000, "tourist_attraction", 1);
-        System.out.println(results.size());
+        List<String> attraction = placeService.searchPlaceIds(34.6937,135.5023, 7000, "tourist_attraction", 1);
+        List<String> restaurant = placeService.searchPlaceIds(34.6937,135.5023, 7000, "restaurant", 1);
+        System.out.println("attraction : "+attraction.toString());
+        System.out.println("restaurant : "+restaurant.toString());
     }
 
     @Test
     void getDetailsByPlaceIdTest() throws IOException {
-        Place results = placeService.getDetailsByPlaceId("ChIJ67mcWJLmAGARrUf0FlFtm7w");
+        Place results = placeService.getDetailsByPlaceId("ChIJI_--Eg3nAGARcvVi6kRKyKM");
         System.out.println(results);
     }
 
+//    @Test
+//    void getDetailsByPlaceIds() throws IOException {
+//        List<String> results = placeService.searchPlaceIds(34.6937,135.5023, 7000, "tourist_attraction", 1);
+//        Map<String, Place> detailsByPlaceIds = placeService.getDetailsByPlaceIds(results);
+//        System.out.println("wyatt: " + detailsByPlaceIds);
+//    }
+
     @Test
     void getDetailsByPlaceIds() throws IOException {
-        List<String> results = placeService.searchPlaceIds(34.6937,135.5023, 7000, "tourist_attraction", 1);
-        Map<String, Place> detailsByPlaceIds = placeService.getDetailsByPlaceIds(results);
+        List<String> results = placeService.searchPlaceIds(34.6937, 135.5023, 7000, "tourist_attraction", 1);
+        Set<String> placeIdSet = new HashSet<>(results);
+        Map<String, Place> detailsByPlaceIds = placeService.getDetailsByPlaceIds(placeIdSet);
         System.out.println("wyatt: " + detailsByPlaceIds);
     }
 }
+
