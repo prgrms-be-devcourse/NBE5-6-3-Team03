afdf105c3bf474923f806886d2ae397dc6847b71
hyeunS-P <hseric0816@gmail.com>
2025-05-14 10:27:41 +0900

    feat: 관리자 페이지 추가 및 mapping

---
+++ src/main/java/grepp/NBE5_6_2_Team03/api/controller/admin/AdminController.java
@@ -0,0 +1,16 @@
+package grepp.NBE5_6_2_Team03.api.controller.admin;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/admin")
+public class AdminController {
+
+    @GetMapping("/dashboard")
+    public String dashboard(Model model) {
+        return "admin-dashboard";
+    }
+}
