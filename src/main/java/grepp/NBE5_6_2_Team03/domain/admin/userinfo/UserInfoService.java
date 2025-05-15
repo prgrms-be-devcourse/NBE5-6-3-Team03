00368fb9f1e9ed8b1d5917e78b5cada3defb6769
NoviceWyatt19 <aseltula596@gmail.com>
2025-05-15 11:36:55 +0900

    feature: 데시보드 연결 및 유저 정보 조회 페이지에 페이지 네이션 추가

--- src/main/java/grepp/NBE5_6_2_Team03/domain/admin/userinfo/UserInfoService.java
+++ src/main/java/grepp/NBE5_6_2_Team03/domain/admin/userinfo/UserInfoService.java
@@ -5,6 +5,8 @@ import grepp.NBE5_6_2_Team03.domain.admin.userinfo.repository.UserInfoRepository
 import java.util.List;
 import java.util.stream.Collectors;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -13,11 +15,16 @@ public class UserInfoService {
 
     private final UserInfoRepository userInfoRepository;
 
-    public List<UserInfoResponse> findAll() {
-        List<UserInfo> userInfos = userInfoRepository.findAll();
-        return userInfos.stream()
-            .map(this::convertToResponse).collect(Collectors.toList());
+    public Page<UserInfoResponse> findAll(Pageable pageable) {
+        Page<UserInfo> userInfos = userInfoRepository.findAll(pageable);
+        return userInfos.map(this::convertToResponse);
     }
+
+//    public List<UserInfoResponse> findAll() {
+//        List<UserInfo> userInfos = userInfoRepository.findAll();
+//        return userInfos.stream()
+//            .map(this::convertToResponse).collect(Collectors.toList());
+//    }
 
     public UserInfoResponse convertToResponse(UserInfo userInfo) {
         return new UserInfoResponse(
