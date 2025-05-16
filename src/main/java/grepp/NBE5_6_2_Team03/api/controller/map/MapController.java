02a5f9244e004f447900bc94319772c17947c109
NoviceWyatt19 <aseltula596@gmail.com>
2025-05-16 22:15:33 +0900

    feature: 여행지 추천페이지 기능

--- src/main/java/grepp/NBE5_6_2_Team03/api/controller/map/MapController.java
+++ src/main/java/grepp/NBE5_6_2_Team03/api/controller/map/MapController.java
@@ -1,16 +1,59 @@
 package grepp.NBE5_6_2_Team03.api.controller.map;
 
+import grepp.NBE5_6_2_Team03.api.controller.map.dto.MapResponse;
 import grepp.NBE5_6_2_Team03.domain.map.MapService;
+import java.util.List;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 @RequiredArgsConstructor
-@RequestMapping("/recommend")
 public class MapController {
 
     private final MapService mapService;
 
+    @GetMapping("/map")
+    public String getMapPage(Model model,
+        @RequestParam(required = false) String countryRequest,
+        @RequestParam(required = false) String cityRequest,
+        @RequestParam(defaultValue = "0") int page,
+        @RequestParam(defaultValue = "20") int size) {
+
+        int minPageLimit = Math.max(0, page);
+        Pageable pageable = PageRequest.of(minPageLimit, size);
+        Page<MapResponse> mapPage = mapService.findPlaces(countryRequest, cityRequest, pageable);
+
+        List<String> countries = mapService.getCountries();
+        List<String> cities;
+        if (hasCityRequest(countryRequest, cityRequest)) {
+            countryRequest = mapService.getCountryByCity(cityRequest).orElse(null);
+        }
+        if (hasCountryRequest(countryRequest)) {
+            cities = mapService.getCity(countryRequest);
+        } else {
+            cities = mapService.getCities();
+        }
+
+        model.addAttribute("mapPage", mapPage);
+        model.addAttribute("countries", countries);
+        model.addAttribute("cities", cities);
+
+        return "map/map";
+    }
+
+    private boolean hasCityRequest(String countryRequest, String cityRequest) {
+        return cityRequest != null && (countryRequest == null || countryRequest.isEmpty());
+    }
+
+    private boolean hasCountryRequest(String countryRequest) {
+        return countryRequest != null && !countryRequest.isEmpty();
+    }
 
 }
