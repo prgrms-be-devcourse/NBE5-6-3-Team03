cc955a12b9f1baf1f76c3d4c569a9f1890e1d721
NoviceWyatt19 <aseltula596@gmail.com>
2025-05-19 11:36:25 +0900

    feature: 관리자 통계기능 추가

--- src/main/java/grepp/NBE5_6_2_Team03/domain/travelplan/repository/TravelPlanRepository.java
+++ src/main/java/grepp/NBE5_6_2_Team03/domain/travelplan/repository/TravelPlanRepository.java
@@ -3,8 +3,18 @@ package grepp.NBE5_6_2_Team03.domain.travelplan.repository;
 import grepp.NBE5_6_2_Team03.domain.travelplan.TravelPlan;
 import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface TravelPlanRepository extends JpaRepository<TravelPlan, Long> {
 
     List<TravelPlan> findByUserId(Long userId);
+
+    @Query("select t.country, count(*) from TravelPlan t group by t.country")
+    List<Object[]> getCountriesStatistics();
+
+    @Query(value = "select month(t.travel_start_date), count(*) from travel_plan t"
+        + " where year(t.travel_start_date) = year(CURDATE()) "
+        + "group by month(t.travel_start_date) order by month(t.travel_start_date)", nativeQuery = true)
+    List<Object[]> getMonthStatistics();
+
 }
