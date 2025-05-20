12d16fcb70422b302ac39e4ddf2529636edb32b1
pia01190 <piakim0119@gmail.com>
2025-05-20 12:17:18 +0900

    fix: getTotalPayedPrice n+1 문제 해결

--- src/main/java/grepp/NBE5_6_2_Team03/domain/expense/repository/ExpenseRepository.java
+++ src/main/java/grepp/NBE5_6_2_Team03/domain/expense/repository/ExpenseRepository.java
@@ -3,10 +3,15 @@ package grepp.NBE5_6_2_Team03.domain.expense.repository;
 import grepp.NBE5_6_2_Team03.domain.expense.Expense;
 import grepp.NBE5_6_2_Team03.domain.travelschedule.TravelSchedule;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.Optional;
 
 public interface ExpenseRepository extends JpaRepository<Expense, Long> {
 
     Optional<Expense> findByTravelSchedule(TravelSchedule travelSchedule);
+
+    @Query("select coalesce(sum(e.payedPrice), 0) from Expense e where e.travelSchedule.travelPlan.travelPlanId = :travelPlanId")
+    int sumPayedPriceByPlanId(@Param("travelPlanId") Long travelPlanId);
 }
