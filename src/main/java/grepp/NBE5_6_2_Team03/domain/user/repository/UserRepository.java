77275f1503bb4a46f835bcea9ec9431e23a9e45c
Wyatt <155226507+NoviceWyatt19@users.noreply.github.com>
2025-06-13 14:13:21 +0900

    refactor: UserCustomRepository 삭제

--- src/main/java/grepp/NBE5_6_2_Team03/domain/user/repository/UserRepository.java
+++ src/main/java/grepp/NBE5_6_2_Team03/domain/user/repository/UserRepository.java
@@ -6,7 +6,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
-public interface UserRepository extends JpaRepository<User, Long>, UserCustomRepository {
+public interface UserRepository extends JpaRepository<User, Long> {
     Optional<User> findByEmail(String email);
     Optional<User> findByName(String name);
 
