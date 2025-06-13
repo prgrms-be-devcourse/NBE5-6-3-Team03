f41cc93a6625d0ed01f94431791745b08ba67ba9
hyeunS-P <hseric0816@gmail.com>
2025-06-13 12:04:59 +0900

    refactor: userMailService 코드 가독성 개선

--- tripin-service/src/main/java/grepp/NBE5_6_2_Team03/domain/mail/service/UserMailService.java
+++ tripin-service/src/main/java/grepp/NBE5_6_2_Team03/domain/mail/service/UserMailService.java
@@ -6,15 +6,15 @@ import grepp.NBE5_6_2_Team03.domain.user.User;
 import grepp.NBE5_6_2_Team03.domain.user.repository.UserRepository;
 import grepp.NBE5_6_2_Team03.global.exception.Message;
 import grepp.NBE5_6_2_Team03.global.exception.NotFoundException;
+import java.time.Duration;
+import java.util.Collections;
+import java.util.Map;
+import java.util.Optional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-
-import java.time.Duration;
-import java.util.Collections;
-import java.util.Map;
 
 @Transactional(readOnly = true)
 @RequiredArgsConstructor
@@ -31,26 +31,25 @@ public class UserMailService {
         String key = "tempPw:" + email;
         String value = "requested";
 
-        if(redisTemplate.hasKey(key)){
+        if (redisTemplate.hasKey(key)) {
             return Collections.singletonMap("success", false);
         }
 
         redisTemplate.opsForValue().set(key, value, Duration.ofMinutes(5));
 
         User user = userRepository.findByEmail(email)
-                .orElseThrow(() -> new NotFoundException(Message.USER_NOT_FOUND));
+            .orElseThrow(() -> new NotFoundException(Message.USER_NOT_FOUND));
 
         CodeMailResponse response = mailServiceClient.sendCode(email, CodeType.PASSWORD).block();
+        boolean result = Optional.ofNullable(response)
+            .map(CodeMailResponse::code)
+            .map(password -> {
+                user.modifyPassword(password);
+                user.updateIsLocked(false);
+                return true;
+            })
+            .orElse(false);
+        return Collections.singletonMap("success", result);
 
-        if(response != null && response.code() != null){
-            String temporaryPassword = response.code();
-
-            user.modifyPassword(passwordEncoder.encode(temporaryPassword));
-            user.updateIsLocked(false);
-            userRepository.save(user);
-            return Collections.singletonMap("success", true);
-        }else{
-            return Collections.singletonMap("success", false);
-        }
     }
 }
