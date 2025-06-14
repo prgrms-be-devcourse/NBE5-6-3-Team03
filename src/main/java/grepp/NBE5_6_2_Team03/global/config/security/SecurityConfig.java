e188fada6d3cf6c9aaedb1da091bad9d33d26758
mikm7243 <mikm7243@naver.com>
2025-06-14 14:53:40 +0900

    feat: 세션 쿠키 -> JWT 토큰 변환
    - same site 정책으로 인해 cross-origin 상태에서 쿠키 세션 사용 불가

--- src/main/java/grepp/NBE5_6_2_Team03/global/config/security/SecurityConfig.java
+++ src/main/java/grepp/NBE5_6_2_Team03/global/config/security/SecurityConfig.java
@@ -1,16 +1,22 @@
 package grepp.NBE5_6_2_Team03.global.config.security;
 
-import grepp.NBE5_6_2_Team03.domain.user.service.CustomUserDetailsService;
+import grepp.NBE5_6_2_Team03.domain.user.repository.UserRepository;
+import grepp.NBE5_6_2_Team03.global.jwt.JwtFilter;
+import grepp.NBE5_6_2_Team03.global.jwt.JwtUtil;
+import grepp.NBE5_6_2_Team03.global.jwt.LoginFilter;
 import jakarta.servlet.http.HttpServletRequest;
-import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 
@@ -18,18 +24,24 @@ import java.util.List;
 
 @Configuration
 @EnableWebSecurity
-@RequiredArgsConstructor
 public class SecurityConfig {
 
-    private final CustomUserDetailsService userDetailsService;
-    private final CustomLoginSuccessHandler loginSuccessHandler;
-    private final CustomLoginFailureHandler loginFailureHandler;
+    private final RedisTemplate<String, String> redisTemplate;
+    private final JwtUtil jwtUtil;
+    private final AuthenticationConfiguration authenticationConfiguration;
+    private final UserRepository userRepository;
+
+    public SecurityConfig(@Qualifier("redisTemplate") RedisTemplate<String, String> redisTemplate, JwtUtil jwtUtil,
+                          AuthenticationConfiguration authenticationConfiguration, UserRepository userRepository) {
+        this.redisTemplate = redisTemplate;
+        this.jwtUtil = jwtUtil;
+        this.authenticationConfiguration = authenticationConfiguration;
+        this.userRepository = userRepository;
+    }
 
     @Bean
-    public AuthenticationManager authenticationManager(HttpSecurity http) throws Exception {
-        AuthenticationManagerBuilder builder = http.getSharedObject(AuthenticationManagerBuilder.class);
-        builder.userDetailsService(userDetailsService).passwordEncoder(bCryptPasswordEncoder());
-        return builder.build();
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration configuration) throws Exception{
+        return configuration.getAuthenticationManager();
     }
 
     @Bean
@@ -45,11 +57,10 @@ public class SecurityConfig {
 
                     @Override
                     public CorsConfiguration getCorsConfiguration(HttpServletRequest request) {
-
                         CorsConfiguration config = new CorsConfiguration();
 
                         config.setAllowedOriginPatterns(List.of("https://v0-hello-in-korean.vercel.app"));
-                        config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+                        config.setAllowedMethods(List.of("GET", "POST", "PUT", "PATCH","DELETE", "OPTIONS"));
                         config.setAllowedHeaders(List.of("*"));
                         config.setAllowCredentials(true);
                         config.setMaxAge(3600L);
@@ -57,28 +68,36 @@ public class SecurityConfig {
                     }
                 }));
 
-        http.csrf(csrf -> csrf.disable());
-
         http
                 .authorizeHttpRequests(
                         (auth) -> auth
                                 .requestMatchers("/css/**", "/assets/**", "/js/**").permitAll()
                                 .requestMatchers("/", "/map/**", "/users/**", "/users/email/**").permitAll()
-                                .requestMatchers("/api/**", "/mail/send", "/users/home", "/plan/**", "/users/my-page").hasRole("USER")
+                                .requestMatchers("/travel-plans", "/travel-plans/**").hasRole("USER")
+                                .requestMatchers("/api/**", "/mail/send", "/users/home", "/travel-plans", "/users/my-page").hasRole("USER")
                                 .requestMatchers("/admin/**").hasRole("ADMIN")
                                 .anyRequest().authenticated()
                 );
 
+        http.csrf(csrf -> csrf.disable());
+
         http
-                .formLogin(auth -> auth
-                        .loginPage("https://v0-hello-in-korean.vercel.app")
-                        .loginProcessingUrl("/users/login-process")
-                        .usernameParameter("email")
-                        .passwordParameter("password")
-                        .successHandler(loginSuccessHandler)
-                        .failureHandler(loginFailureHandler)
-                        .permitAll()
-                );
+                .formLogin((auth) -> auth.disable());
+
+        UsernamePasswordAuthenticationFilter loginFilter = new LoginFilter(
+                redisTemplate, userRepository, authenticationManager(authenticationConfiguration), jwtUtil
+        );
+        loginFilter.setFilterProcessesUrl("/users/login-process");
+
+        http
+                .addFilterAt(loginFilter, UsernamePasswordAuthenticationFilter.class);
+
+        http
+                .addFilterBefore(new JwtFilter(jwtUtil), LoginFilter.class);
+
+        http
+                .sessionManagement((session) -> session
+                        .sessionCreationPolicy(SessionCreationPolicy.STATELESS));
 
         http.logout(logout -> logout
                 .logoutUrl("/users/logout")
