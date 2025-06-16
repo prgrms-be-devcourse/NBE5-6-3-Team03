a5c3235f40c89e4a57c84f4c86a32f7dde3f5fd5
Wyatt <155226507+NoviceWyatt19@users.noreply.github.com>
2025-06-16 16:45:53 +0900

    refactor: getPlaces PostMapping 으로 변경

--- tripin-service/src/main/java/grepp/NBE5_6_2_Team03/api/controller/admin/PlacesController.java
+++ tripin-service/src/main/java/grepp/NBE5_6_2_Team03/api/controller/admin/PlacesController.java
@@ -15,6 +15,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -25,7 +26,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class PlacesController {
     private final PlaceService placeService;
 
-    @GetMapping("/places")
+    @PostMapping("/places")
     public ApiResponse<PlaceInfoListResponse> getPlaces(@RequestBody PlaceSearchRequest searchRequest ) {
         Page<PlaceInfoResponse> places = placeService.findPlacesPage(searchRequest);
         CountryCityInfo countryCityInfo = placeService.getCountryCityInfo();
