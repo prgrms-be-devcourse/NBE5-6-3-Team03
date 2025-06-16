16b619147553fa47b6ac1e941b75a01acd67c62b
Wyatt <155226507+NoviceWyatt19@users.noreply.github.com>
2025-06-16 15:23:42 +0900

    refactor: places 요청 시 기본적으로 생성되게 변경

--- tripin-service/src/main/java/grepp/NBE5_6_2_Team03/api/controller/admin/dto/place/request/PlaceSearchRequest.java
+++ tripin-service/src/main/java/grepp/NBE5_6_2_Team03/api/controller/admin/dto/place/request/PlaceSearchRequest.java
@@ -10,20 +10,25 @@ import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 
 @Getter
-@AllArgsConstructor
 @Builder
 public class PlaceSearchRequest {
     private String country;
     private String city;
 
-    // Pageable 로 묶으면 여기서 Validation 불가능
     @Min(value = 0, message = "페이지는 0 이상이어야 합니다")
     @Max(value = 200, message = "페이지는 최댓값이 200입니다")
-    private int page;
+    private Integer page;
 
     @Min(value = 1, message = "페이지 크기는 1 이상이어야 합니다")
     @Max(value = 30, message = "페이지 크기는 30이하여야 합니다")
-    private int size;
+    private Integer size;
+
+    public PlaceSearchRequest(String country, String city, Integer page, Integer size) {
+        this.country = (country == null) ? "" : country;
+        this.city = (city == null) ? "" : city;
+        this.page = (page == null) ? 0 : page;
+        this.size = (size == null) ? 30 : size;
+    }
 
     public Pageable getPageable() {
         return PageRequest.of(this.page, this.size);
