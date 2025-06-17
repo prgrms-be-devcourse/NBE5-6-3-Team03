9d15164c2c78497ffa346f8f364fd7db6d74ac35
pia01190 <piakim0119@gmail.com>
2025-06-17 14:40:38 +0900

    refactor: exchange_rate.sql 추가

--- tripin-service/src/main/java/grepp/NBE5_6_2_Team03/global/config/web/RestTemplateConfig.java
+++ tripin-service/src/main/java/grepp/NBE5_6_2_Team03/global/config/web/RestTemplateConfig.java
@@ -2,46 +2,13 @@ package grepp.NBE5_6_2_Team03.global.config.web;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.client.SimpleClientHttpRequestFactory;
 import org.springframework.web.client.RestTemplate;
-
-import javax.net.ssl.*;
-import java.security.cert.X509Certificate;
 
 @Configuration
 public class RestTemplateConfig {
     @Bean
     public RestTemplate restTemplate() {
-        return createUnsafeRestTemplate();
-    }
-
-    private RestTemplate createUnsafeRestTemplate() {
-        try {
-            TrustManager[] trustAllCerts = new TrustManager[]{
-                new X509TrustManager() {
-                    public X509Certificate[] getAcceptedIssuers() { return new X509Certificate[0]; }
-                    public void checkClientTrusted(X509Certificate[] certs, String authType) {}
-                    public void checkServerTrusted(X509Certificate[] certs, String authType) {}
-                }
-            };
-
-            SSLContext sslContext = SSLContext.getInstance("TLS");
-            sslContext.init(null, trustAllCerts, new java.security.SecureRandom());
-            HttpsURLConnection.setDefaultSSLSocketFactory(sslContext.getSocketFactory());
-
-            HostnameVerifier allHostsValid = (hostname, session) -> true;
-            HttpsURLConnection.setDefaultHostnameVerifier(allHostsValid);
-
-            SimpleClientHttpRequestFactory requestFactory = new SimpleClientHttpRequestFactory();
-            RestTemplate restTemplate = new RestTemplate(requestFactory);
-            restTemplate.getInterceptors().add((request, body, execution) -> {
-                request.getHeaders().add("User-Agent", "Mozilla/5.0");
-                return execution.execute(request, body);
-            });
-            return restTemplate;
-        } catch (Exception e) {
-            throw new RuntimeException("Failed to create unsafe RestTemplate", e);
-        }
+        return new RestTemplate();
     }
 
 }
